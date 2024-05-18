ec3d717575b2970fb9a0cac388306589c2084f7a
francisco <frnncs@gmail.com>
2024-05-18 13:17:22 -0300

    agrego models al admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from main.models import Contract, Payment, Product, RecurrentContract
+
+admin.site.register(Product)
+admin.site.register(Contract)
+admin.site.register(RecurrentContract)
+admin.site.register(Payment)
